5742a36433fb7d6a85fad07112f2a9e66345ab8e
AdamBremler <adambremler@hotmail.com>
2019-02-21 23:21:19 +0100

    Add JWT and add API routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', 'ApiController@login');
+Route::post('register', 'ApiController@register');
+ 
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('logout', 'ApiController@logout');
+ 
+    Route::get('user', 'ApiController@getAuthUser');
+ 
+    Route::get('lists', 'ListController@index');
+    Route::get('lists/{id}', 'ListController@show');
+    Route::post('lists', 'ListController@store');
+    Route::put('lists/{id}', 'ListController@update');
+    Route::delete('lists/{id}', 'ListController@destroy');
+});
